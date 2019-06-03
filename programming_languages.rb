cc2424514bec07a6227f0d7f6fbcf74d407628c2
Conrad Casper <conradcasper@icloud.com>
2019-06-03 18:32:19 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,16 +1,33 @@
+require "pry"
+
+# def reformat_languages(languages)
+#   new_hash = Hash.new
+  
+#   languages.each do |style, languages|
+    
+#     languages.each do |language, type|
+#       binding.pry
+#     if new_hash.keys.include?(language)
+#         new_hash[language][:style] << style
+#       else 
+#         new_hash[language] = type
+#         new_hash[language][:style] = [style]
+        
+#       end
+#     end
+#   end
+#   new_hash
+# end
+
+
 def reformat_languages(languages)
-  # your code here
   new_hash = {}
   
-  languages.each do |style, langs|
-    langs.each do |language, type|
-      if new_hash.keys.include?(language)
-        new_hash[language][:style] << style
-      else 
-        new_hash[language] = type
-        new_hash[language][:style] = [style]
-      end
-    end
+  languages.each do |style, languages|
+    languages.each do |language, type|
+      binding.pry
+   end
+  end
 end
-new_hash
-end
+  
+
